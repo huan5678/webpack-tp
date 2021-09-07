f8c0da4cceb26ca98eceeaf9dde4e0b20f1441f8
huan5678 <huan5678@gmail.com>
2021-09-07 14:58:11 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import _ from "lodash";
+
+const demoName = "12345 Config";
+
+function component() {
+  const element = document.createElement("div");
+
+  element.innerHTML = _.join(["Webpack Demo", demoName], ": ");
+
+  return element;
+}
+
+document.body.appendChild(component());
